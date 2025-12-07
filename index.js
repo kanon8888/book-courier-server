59123463f3722294d7875710233a9ba03d511041
kanon8888 <mdrakibhasankanon@gmail.com>
2025-12-08 04:17:24 +0600

    two commiy

--- index.js
+++ index.js
@@ -27,7 +27,47 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const db = client.db('book_courier');
+        const booksCollection = client.db("bookCourierDB").collection("books");
+        // const ordersCollection = client.db("bookCourierDB").collection("orders");
+        // const usersCollection = client.db("bookCourierDB").collection("users");
+        // const paymentsCollection = client.db("bookCourierDB").collection("payments");
 
+        // parcel api
+        app.get('/book', async (req, res) => {
+
+        })
+
+        app.post('/book', async (req, res) => {
+            const parcel = req.body;
+            const result = await booksCollection.insertOne(parcel);
+            res.send(result)
+        })
+
+        app.post("/books", async (req, res) => {
+            const book = req.body;
+
+            // Default status (published/unpublished)
+            if (!book.status) {
+                book.status = "published";
+            }
+
+            const result = await booksCollection.insertOne(book);
+            res.send(result);
+        });
+
+
+        app.get("/books", async (req, res) => {
+            const result = await booksCollection.find({ status: "published" }).toArray();
+            res.send(result);
+        });
+
+
+        app.get("/books/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await booksCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
 
 
         // Send a ping to confirm a successful connection
